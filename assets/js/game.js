b371fd577f0c7ca9476fbda36f5ef12ed5aa5802
Sophie <sophiehickey123@gmail.com>
2020-05-25 16:39:57 +0000

    used DirtyMarkup Beautifier to fix format

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,178 +1,137 @@
-const diffChoice = document.getElementById("difficultySelect");
-const quantChoice = document.getElementById("questionSelect");
-const catId = document.getElementById("submitCat");
-const game = document.getElementById("theGame");
-const question = document.getElementById("question");
-const start = document.getElementById("ReadyToPlay");
+const diffChoice = document.getElementById( "difficultySelect" );
+const quantChoice = document.getElementById( "questionSelect" );
+const catId = document.getElementById( "submitCat" );
+const game = document.getElementById( "theGame" );
+const question = document.getElementById( "question" );
+const start = document.getElementById( "ReadyToPlay" );
+const catChoice = document.getElementById( "categoryList" );
+const choices = Array.from( document.getElementsByClassName( "choice-text" ) );
+const progressText = document.getElementById( "progressText" );
+const scoreText = document.getElementById( "score" );
+const progressbarfull = document.getElementById( "progressbarfull" );
+const end = document.getElementById( "gameOver" );
 
-const catChoice = document.getElementById("categoryList");
-
-const choices = Array.from(document.getElementsByClassName("choice-text"));
-const progressText = document.getElementById("progressText");
-const scoreText = document.getElementById("score");
-const progressbarfull = document.getElementById("progressbarfull");
-const end = document.getElementById("gameOver");
-
-
-
-//variables
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
-let availableQuestions = []; 
-
-let questions = []; 
-let base_URL = "https://opentdb.com/";
+let availableQuestions = [];
+let questions = [];
+let baseUrl = "https://opentdb.com/";
 
 // Fetches category list from API
-function getData(gameTrigger) {
-    if (gameTrigger) {
-        dataUrl = (`${base_URL}api.php?amount=${quant}&category=${id}&difficulty=${diff}&type=multiple`);
-    } else {
-        dataUrl = (`${base_URL}api_category.php`);
-    }
+function getData( gameTrigger ) {
+	if ( gameTrigger ) {
+		dataUrl = ( `${baseUrl}api.php?amount=${quant}&category=${id}&difficulty=${diff}&type=multiple` );
+	} else {
+		dataUrl = ( `${baseUrl}api_category.php` );
+	}
 }
-
 //Gets the categories and pass to DOM
 function categories() {
-    getData(false);
-    fetch(dataUrl)
-        .then(response => response.json())
-        .then(category => {
-            let categoryList = category.trivia_categories;
-            categoryList.forEach(category => {
-
-                let categoryOption = document.createElement("option");
-                let categoryName = document.createElement("p");
-                let name = document.createTextNode(category.name);
-
-                categoryName.appendChild(name);
-                categoryOption.appendChild(categoryName);
-                categoryOption.id = category.id;
-                categoryOption.classList.add("category");
-                document.getElementById("categoryList").appendChild(categoryOption);
-            });
-            start.classList.remove("hide");
-        })
-        .catch(() => console.error());
+	getData( false );
+	fetch( dataUrl ).then( response => response.json() ).then( category => {
+		let categoryList = category.trivia_categories;
+		categoryList.forEach( category => {
+			let categoryOption = document.createElement( "option" );
+			let categoryName = document.createElement( "p" );
+			let name = document.createTextNode( category.name );
+			categoryName.appendChild( name );
+			categoryOption.appendChild( categoryName );
+			categoryOption.id = category.id;
+			categoryOption.classList.add( "category" );
+			document.getElementById( "categoryList" ).appendChild( categoryOption );
+		} );
+		start.classList.remove( "hide" );
+	} ).catch( () => console.error() );
 }
-
 categories();
 
-
-
 function getQuiz() {
-    getData(true);
-        fetch(dataUrl)
-        .then(data => data.json())
-        .then(loadedQuestions => {
-           /*  console.log(loadedQuestions.results);  */
-            questions = loadedQuestions.results.map(loadedQuestion => {
-                const formattedQuestion = {
-                    question: loadedQuestion.question
-                };
-
-                
-            formattedQuestion.answer = Math.floor(Math.random() * 3) +1;
-            const answerChoices = [...loadedQuestion.incorrect_answers];
-            answerChoices.splice(formattedQuestion.answer -1, 0, loadedQuestion.correct_answer);
-        
-            answerChoices.forEach((choice, index) => {
-                formattedQuestion['choice' + (index +1)] = choice;
-            });
-            return formattedQuestion;
-        });
-        startGame()
-  })
-  .catch(err => {
-      console.log(err);
-  });
+	getData( true );
+	fetch( dataUrl ).then( data => data.json() ).then( loadedQuestions => {
+		questions = loadedQuestions.results.map( loadedQuestion => {
+			const formattedQuestion = {
+				question: loadedQuestion.question
+			};
+			formattedQuestion.answer = Math.floor( Math.random() * 3 ) + 1;
+			const answerChoices = [ ...loadedQuestion.incorrect_answers ];
+			answerChoices.splice( formattedQuestion.answer - 1, 0, loadedQuestion.correct_answer );
+			answerChoices.forEach( ( choice, index ) => {
+				formattedQuestion[ 'choice' + ( index + 1 ) ] = choice;
+			} );
+			return formattedQuestion;
+		} );
+		startGame()
+	} ).catch( err => {
+		console.log( err );
+	} );
 }
-  
-
-
 //constants
 const correct_bonus = 10;
 const max_questions = 10;
-const subtract_value = -2; 
-
+const subtract_value = -2;
 startGame = () => {
-    questionCounter = 0;
-    score = 0;
-    availableQuestions = [...questions];
-    getNewQuestion();
-    game.classList.remove("hide");
+	questionCounter = 0;
+	score = 0;
+	availableQuestions = [ ...questions ];
+	getNewQuestion();
+	game.classList.remove( "hide" );
 };
-
 getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionCounter >= max_questions) {
-        localStorage.setItem("mostRecentScore", score);
-        game.classList.add("hide");
-        end.classList.remove("hide");
-    //When game over will go to the end game html
-    }
-
-    questionCounter++;
-    progressText.innerText = `Question ${questionCounter}/${max_questions}`;
-    //update progress bar
-    progressbarfull.style.width = `${(questionCounter / max_questions) * 100}%`;
-
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-        currentQuestion = availableQuestions[questionIndex];
-        question.innerText = currentQuestion.question;
-
-    choices.forEach(choice => {
-        const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number];
-    });
-
-    // splice will remove the answer we just picked so we are not choosing it again
-    availableQuestions.splice(questionIndex, 1);
-    acceptingAnswers = true;
-    };
-
-choices.forEach(choice => {
-    choice.addEventListener("click", e => {
-        if(!acceptingAnswers) return;
-
-        acceptingAnswers = false;
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset["number"];
-
-        const classToApply = selectedAnswer == currentQuestion.answer ? 'correct': 'incorrect';
-        
-        if (classToApply === "correct") {
-            incrementScore(correct_bonus);
-         }  else {
-             incrementScore(subtract_value);
-             console.log(subtract_value)
-            };
-
-        selectedChoice.parentElement.classList.add(classToApply);
-
-        //this will add a delay between questions
-         setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply);
-            getNewQuestion();
-            }, 1000);
-        });
-});
-
-incrementScore = num => {
-  score += num;
-  scoreText.innerText = score;
+	if ( availableQuestions.length === 0 || questionCounter >= max_questions ) {
+		localStorage.setItem( "mostRecentScore", score );
+		game.classList.add( "hide" );
+		end.classList.remove( "hide" );
+		//When game over will go to the end game html
+	}
+	questionCounter++;
+	progressText.innerText = `Question ${questionCounter}/${max_questions}`;
+	//update progress bar
+	progressbarfull.style.width = `${(questionCounter / max_questions) * 100}%`;
+	const questionIndex = Math.floor( Math.random() * availableQuestions.length );
+	currentQuestion = availableQuestions[ questionIndex ];
+	question.innerText = currentQuestion.question;
+	choices.forEach( choice => {
+		const number = choice.dataset[ 'number' ];
+		choice.innerText = currentQuestion[ 'choice' + number ];
+	} );
+	// splice will remove the answer we just picked so we are not choosing it again
+	availableQuestions.splice( questionIndex, 1 );
+	acceptingAnswers = true;
 };
-
-
-catId.addEventListener('click', () => {
-    id = catChoice.options[catChoice.selectedIndex].id;
-    diff = diffChoice.options[diffChoice.selectedIndex].id;
-    quant = quantChoice.options[quantChoice.selectedIndex].id;
-    start.classList.add("hide");
-    getQuiz();
-}); 
-
+choices.forEach( choice => {
+	choice.addEventListener( "click", e => {
+		if ( !acceptingAnswers ) return;
+		acceptingAnswers = false;
+		const selectedChoice = e.target;
+		const selectedAnswer = selectedChoice.dataset[ "number" ];
+		const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+		if ( classToApply === "correct" ) {
+			incrementScore( correct_bonus );
+		} else {
+			incrementScore( subtract_value );
+			console.log( subtract_value )
+		};
+		selectedChoice.parentElement.classList.add( classToApply );
+		//this will add a delay between questions
+		setTimeout( () => {
+			selectedChoice.parentElement.classList.remove( classToApply );
+			getNewQuestion();
+		}, 1000 );
+	} );
+} );
+incrementScore = num => {
+	score += num;
+	scoreText.innerText = score;
+};
+catId.addEventListener( 'click', () => {
+	id = catChoice.options[ catChoice.selectedIndex ].id;
+	diff = diffChoice.options[ diffChoice.selectedIndex ].id;
+	quant = quantChoice.options[ quantChoice.selectedIndex ].id;
+	start.classList.add( "hide" );
+	getQuiz();
+} );
 /*Countdown Timer
 
 //Questions array
