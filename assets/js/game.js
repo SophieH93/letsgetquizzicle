8d3c3098f6b95b6b4b11413fcfe35663d2bf5340
Sophie <sophiehickey123@gmail.com>
2020-06-09 14:02:33 +0000

    removed highscores, deleted unnecessary code, added countdown -not complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,37 +1,37 @@
- const start = document.querySelector("#ReadyToPlay");
+const start = document.querySelector("#ReadyToPlay");
 const quizCategorySelect = document.querySelector("#categorySelect");
 const difficultySelect = document.querySelector("#difficultySelect");
 const questionAmountSelect = document.querySelector("#quantChoice");
 const submitQuizOptions = document.querySelector("#submitOptions");
 const gamePage = document.querySelector("#theGame");
 const quiteBtn = document.querySelector( "#quitGame");
-const progressText = document.querySelector("#progressText");
+const questionCount = document.querySelector("#questionCount");
 const playersScore = document.querySelector("#score");
 const triviaQuestions = document.querySelector("#question");
-const answerChoices = Array.from( document.getElementsByClassName("choice-text"));
-const QuizEndPg = document.querySelector("#gameOver");
-const scoreBoardBtn = document.querySelector("#scoreBoardBtn");
-const scoreBoardPg  = document.querySelector("#scoreBoard");
+const answerChoices = Array.from( document.getElementsByClassName("answerChoice"));
+const QuizEndPage = document.querySelector("#gameOver");
+//const scoreBoardBtn = document.querySelector("#scoreBoardBtn");
+//const scoreBoardPage  = document.querySelector("#scoreBoard");
 const username = document.querySelector("#username");
-const saveScoreBtn = document.querySelector("#saveScoreBtn");
+//const saveScoreBtn = document.querySelector("#saveScoreBtn");
 const finalscore = document.querySelector("#finalscore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-const highScoresList = document.querySelector("#highScoresList");
+//const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+//const highScoresList = document.querySelector("#highScoresList");
 const logoText = document.querySelector( "#logo" );
-const correct_bonus = 10;
-const subtract_value = -2;
-const max_high_scores = 5;
+const scoreBonus = 10;
+const subtractPoint = 1;
+//const max_high_scores = 5;
 
 let currentQuestion = {};
-let acceptingAnswers = false;
+//let acceptingAnswers = false; //delay user answering until everything is loaded
 let score = 0;
-let questionCounter = 0;
-let availableQuestions = [];
+let questionCounter = 0; //what q are you on
+let availableQuestions = []; //take Q out to gice unique q's
 let questions = [];
 let baseUrl = "https://opentdb.com/";
 let dataUrl;
-let quant;
+let quantity;
 
 
 const homeButton = document.querySelector("#homeBtn");
@@ -40,19 +40,18 @@ const homeButton = document.querySelector("#homeBtn");
 
 /**
  * 
- * @param {boolen} gameTrigger
+ * @param {boolen} triviaAPI
  */
 
 /**
  * Custom URL
  *  
  */ 
-
-const getData = gameTrigger => {
-	if ( gameTrigger ) {
-		dataUrl = ( `${baseUrl}api.php?amount=${quant}&category=${id}&difficulty=${diff}&type=multiple` );
+const getData = triviaAPI => {
+	if (triviaAPI) {
+		dataUrl = (`${baseUrl}api.php?amount=${quantity}&category=${id}&difficulty=${diff}&type=multiple`);
 	} else {
-		dataUrl = ( `${baseUrl}api_category.php` );
+		dataUrl = (`${baseUrl}api_category.php`);
 	}
 }
 
@@ -79,6 +78,9 @@ const categories = () => {
 }
 categories();
 
+/**
+ * Retrieves Quiz Data, sorts the data and passes to the DOM
+ */
 const getQuiz = () =>{    
 	getData( true );
 	fetch( dataUrl ).then( data => data.json() ).then( loadedQuestions => {
@@ -108,47 +110,45 @@ const getQuiz = () =>{
 startGame = () => {
 	questionCounter = 0;
     score = 0;
-    availableQuestions = [...questions];
+    availableQuestions = [...questions]; //spread operator gathers the full questions from the questions array
     gamePage.classList.remove( "hide" );    
-    getNewQuestion();
+    fetchNewQuestion();
 };
 
-
-getNewQuestion = () => {    
-	if ( availableQuestions.length === 0) { 
+/**
+ * Pull questions from array & loads new questions when answer is chosen
+ * Hightlights right and wrong answers
+ * Displays question counter and Score
+ */
+fetchNewQuestion = () => {    
+	if (availableQuestions.length === 0) { 
 		localStorage.setItem( "mostRecentScore", score );
 		gamePage.classList.add( "hide" );
-		QuizEndPg.classList.remove( "hide" );
-		//When game over will go to the end game html
+		QuizEndPage.classList.remove( "hide" );
+		
 	} else {
-	questionCounter++;
-    progressText.innerHTML= `Question: ${questionCounter}/${quant}`;
+	questionCounter++; 
+    questionCount.innerHTML= `Question: ${questionCounter}/${quantity}`;
     playersScore.innerHTML= `Score: ${score}`;            
 	const questionIndex = Math.floor( Math.random() * availableQuestions.length );
 	currentQuestion = availableQuestions[ questionIndex ];
-	question.innerHTML = currentQuestion.question;
+    question.innerHTML = currentQuestion.question;
 	answerChoices.forEach( choice => {
-		const number = choice.dataset[ 'number' ];
-		choice.innerHTML = currentQuestion[ 'choice' + number ];
+		const number = choice.dataset['number'];
+		choice.innerHTML = currentQuestion['choice' + number];
 	} );
-	// splice will remove the answer we just picked so we are not choosing it again
 	availableQuestions.splice( questionIndex, 1 );
-	acceptingAnswers = true;
 }};
-
-answerChoices.forEach( choice => {
-	choice.addEventListener( "click", e => {
-		if ( !acceptingAnswers ) return;
-		acceptingAnswers = false;
-		const selectedChoice = e.target;
-		const selectedAnswer = selectedChoice.dataset[ "number" ];
+answerChoices.forEach(choice => {
+	choice.addEventListener( "click", event => {
+		const selectedAnswerChoice = event.target;
+		const selectedAnswer = selectedAnswerChoice.dataset["number"];
 		const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
 		if ( classToApply === "correct" ) {
-			incrementScore( correct_bonus );
-            
+			incrementScore(scoreBonus);            
 		} else {
-			incrementScore( subtract_value );
-			console.log( subtract_value );
+			incrementScore(subtractPoint);
+			console.log(subtractPoint);
            swal.fire({
                 title: "Wrong Answer!",
                 text: `Sorry the correct answer was number ${currentQuestion.answer}!`,
@@ -156,26 +156,23 @@ answerChoices.forEach( choice => {
                 showConfirmButton: false,
                 timer: 1500
            });
-        };
-		
-		selectedChoice.parentElement.classList.add( classToApply );
-		//this will add a delay between questions
+        };		
+		selectedAnswerChoice.parentElement.classList.add(classToApply);
 		setTimeout( () => {
-			selectedChoice.parentElement.classList.remove( classToApply );
-			getNewQuestion();
-		}, 1000 );
+			selectedAnswerChoice.parentElement.classList.remove(classToApply);
+			fetchNewQuestion();
+		}, 1500 );
 	} );
 } );
-incrementScore = num => {
-	score += num;
-	
+const incrementScore = num => {
+	score += num;	
 };
 
 
 submitQuizOptions.addEventListener( 'click', () => {
 	id = quizCategorySelect.options[ quizCategorySelect.selectedIndex ].id;
 	diff = difficultySelect.options[ difficultySelect.selectedIndex ].id;
-	quant = questionAmountSelect.options[ questionAmountSelect.selectedIndex ].id;
+	quantity = questionAmountSelect.options[ questionAmountSelect.selectedIndex ].id;
 	start.classList.add( "hide" );
 	getQuiz();
 } );
@@ -191,11 +188,11 @@ quiteBtn.addEventListener( 'click', () => {
     start.classList.remove( "hide" );	
 } );
 
-/*When scoreboard btn is clicked hide the endGame page and show the scoreboard page */
+/*When scoreboard btn is clicked hide the endGame page and show the scoreboard page 
 scoreBoardBtn.addEventListener( 'click', () => {
     QuizEndPg.classList.add( "hide" );
     scoreBoardPg.classList.remove( "hide" );	
-} );
+} );*/ 
 
 /**
  * return home
@@ -205,52 +202,25 @@ homeButton.addEventListener( 'click', () => {
     start.classList.remove( "hide" );	    
 } );
 
-/* High Scores */
-highScoresList.innerHTML =
-highScores.map(score => {
-   return `<li class="high-score">${score.name}- ${score.score}</li>`;
-}).join("");
 
 finalscore.innerText = mostRecentScore;
 
-username.addEventListener('keyup', () => {
-    saveScoreBtn.disabled = !username.value;
-    // If no username inputted btn will stay disabled, when username inputted btn becomes visable
-})
 
-savingHighScore = e => {
-    console.log("clicked the save button");
-    e.preventDefault();
-    
-const score = {
-    score: Math.floor(Math.random() * 100),
-    name: username.value
-};
+let counter = 10;
 
-highScores.push(score);
-highScores.sort((a, b) => b.score - a.score)
-highScores.splice(5);
+ setInterval(function()  {
+    counter--;
 
-localStorage.setItem('highScores', JSON.stringify(highScores));
-/*window.location.assign('/'); */
-
-};
-
-/*
-let counter = 10;
-
-setInterval(function() {
-    counter--;
-    if (counter >= 0) {
-        id = document.getElementById('count');
-        id.innerHTML = counter;
-    }
-    else if (counter === 0) {
-            id.innerHTML = 'Times Up!';
-            getNewQuestion();
-            counter = 10;
+    if (counter >= 0) {
+        id = document.querySelector('#count');
+        id.innerHTML = counter;
     }
+     if  (counter === 0) {
+            id.innerHTML = 'Times Up!';           
+            counter = 10;
+            fetchNewQuestion();
+           
+    } 
 
-}, 1000);
+}, 1000); 
 
-*/
