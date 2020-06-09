8d4591443cc8b8424fcf17baaa0fda588c8f9dc3
Sophie <sophiehickey123@gmail.com>
2020-06-09 18:18:52 +0000

    fixed typo error for endheading and finalscore id's

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,4 @@
-const start = document.querySelector("#ReadyToPlay");
+const quizSelectOptions = document.querySelector("#ReadyToPlay");
 const quizCategorySelect = document.querySelector("#categorySelect");
 const difficultySelect = document.querySelector("#difficultySelect");
 const questionAmountSelect = document.querySelector("#quantChoice");
@@ -10,10 +10,7 @@ const playersScore = document.querySelector("#score");
 const triviaQuestions = document.querySelector("#question");
 const answerChoices = Array.from( document.getElementsByClassName("answerChoice"));
 const QuizEndPage = document.querySelector("#gameOver");
-//const scoreBoardBtn = document.querySelector("#scoreBoardBtn");
-//const scoreBoardPage  = document.querySelector("#scoreBoard");
 const username = document.querySelector("#username");
-//const saveScoreBtn = document.querySelector("#saveScoreBtn");
 const finalscore = document.querySelector("#finalscore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 //const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
@@ -21,7 +18,7 @@ const mostRecentScore = localStorage.getItem("mostRecentScore");
 const logoText = document.querySelector( "#logo" );
 const scoreBonus = 10;
 const subtractPoint = 1;
-//const max_high_scores = 5;
+
 
 let currentQuestion = {};
 //let acceptingAnswers = false; //delay user answering until everything is loaded
@@ -47,60 +44,67 @@ const homeButton = document.querySelector("#homeBtn");
  * Custom URL
  *  
  */ 
-const getData = triviaAPI => {
+const getData = triviaAPI =>  {
 	if (triviaAPI) {
-		dataUrl = (`${baseUrl}api.php?amount=${quantity}&category=${id}&difficulty=${diff}&type=multiple`);
+		dataUrl = (`${baseUrl}api.php?amount=${quantity}&category=${categoryList}&difficulty=${levelDifficulty}&type=multiple`);
 	} else {
 		dataUrl = (`${baseUrl}api_category.php`);
 	}
 }
 
-//Gets the categories and pass to DOM
-const categories = () => {   
-	getData( false );
-    fetch( dataUrl )
-    .then( response => response.json())
-    .then( category => {
-		let categorySelect = category.trivia_categories;
-		categorySelect.forEach( category => {
-			let categoryOption = document.createElement( "option" );
-			let categoryName = document.createElement( "p" );
-			let name = document.createTextNode( category.name );
-			categoryName.appendChild( name );
-			categoryOption.appendChild( categoryName );
-			categoryOption.id = category.id;
-			categoryOption.classList.add( "category" );
-			document.getElementById( "categorySelect" ).appendChild( categoryOption );
-        } );        
-        logoText.classList.remove("hide");
-        start.classList.remove( "hide" );           
-	} ).catch( () => console.error() );
-}
-categories();
 
 /**
- * Retrieves Quiz Data, sorts the data and passes to the DOM
+ * Fetches the categories from the API and passes to the DOM
+ */
+const apiCategories = () => {   
+	getData(false);
+    fetch(dataUrl)
+    .then(response => response.json())
+    .then(category => {
+		let categorySelect = category.trivia_categories;
+		categorySelect.forEach(category => {
+		    let categoryOption = document.createElement("option");
+			let categoryHeadings = document.createElement("p");
+			let categoryName = document.createTextNode(category.name);
+			categoryHeadings.appendChild(categoryName);
+			categoryOption.appendChild(categoryHeadings);
+			categoryOption.id = category.id;
+			categoryOption.classList.add("category");
+			document.getElementById("categorySelect").appendChild(categoryOption);
+        } );        
+        quizSelectOptions.classList.remove("hide");   
+          
+    } )
+    .catch( () => console.error() );
+}
+apiCategories();
+
+/**
+ * Pulls questions from the api & Formats the questions and answers
  */
 const getQuiz = () =>{    
-	getData( true );
-	fetch( dataUrl ).then( data => data.json() ).then( loadedQuestions => {
-		questions = loadedQuestions.results.map( loadedQuestion => {
-			const formattedQuestion = {
+getData(true);
+    fetch(dataUrl)
+    .then(data => data.json() )
+    .then(loadedApiQuestions => {
+		questions = loadedApiQuestions.results.map(loadedQuestion => {
+			const formatQuestion = {
 				question: loadedQuestion.question
 			};
-			formattedQuestion.answer = Math.floor( Math.random() * 3 ) + 1;
-			const answerChoices = [ ...loadedQuestion.incorrect_answers ];
-			answerChoices.splice( formattedQuestion.answer - 1, 0, loadedQuestion.correct_answer );
-			answerChoices.forEach( ( choice, index ) => {
-				formattedQuestion[ 'choice' + ( index + 1 ) ] = choice;
+			formatQuestion.answer = Math.floor( Math.random() * 3 ) + 1;
+			const answerSelectChoice = [...loadedQuestion.incorrect_answers];
+			answerSelectChoice.splice(formatQuestion.answer - 1, 0, loadedQuestion.correct_answer);
+			answerSelectChoice.forEach((choice, index) => {
+				formatQuestion['choice' + (index + 1)] = choice;
 			} );
-			return formattedQuestion;
+			return formatQuestion;
         } );
-         logoText.classList.add("hide")
+      
+         logoText.classList.add("hide");
         startGame()
        
 	} ).catch( err => {
-		console.log( err );
+		console.error( err );
 	} );
 }
 
@@ -111,7 +115,7 @@ startGame = () => {
 	questionCounter = 0;
     score = 0;
     availableQuestions = [...questions]; //spread operator gathers the full questions from the questions array
-    gamePage.classList.remove( "hide" );    
+    gamePage.classList.remove( "hide" );  
     fetchNewQuestion();
 };
 
@@ -170,57 +174,27 @@ const incrementScore = num => {
 
 
 submitQuizOptions.addEventListener( 'click', () => {
-	id = quizCategorySelect.options[ quizCategorySelect.selectedIndex ].id;
-	diff = difficultySelect.options[ difficultySelect.selectedIndex ].id;
+	categoryList = quizCategorySelect.options[ quizCategorySelect.selectedIndex ].id;
+	levelDifficulty = difficultySelect.options[ difficultySelect.selectedIndex ].id;
 	quantity = questionAmountSelect.options[ questionAmountSelect.selectedIndex ].id;
-	start.classList.add( "hide" );
+	quizSelectOptions.classList.add( "hide" );
 	getQuiz();
 } );
 
-
-
-/**
- * Quite game & return home
- * */
-
-quiteBtn.addEventListener( 'click', () => {
-    gamePage.classList.add( "hide" );
-    start.classList.remove( "hide" );	
+quiteBtn.addEventListener('click', () => {
+    gamePage.classList.add("hide");
+    quizSelectOptions.classList.remove("hide");	
+    logoText.classList.remove("hide");
 } );
 
-/*When scoreboard btn is clicked hide the endGame page and show the scoreboard page 
-scoreBoardBtn.addEventListener( 'click', () => {
-    QuizEndPg.classList.add( "hide" );
-    scoreBoardPg.classList.remove( "hide" );	
-} );*/ 
-
-/**
- * return home
- * */
-homeButton.addEventListener( 'click', () => {  
-    gameOver.classList.add( "hide" );
-    start.classList.remove( "hide" );	    
+homeButton.addEventListener('click', () => {  
+    QuizEndPage.classList.add("hide");
+    logoText.classList.remove("hide");  
+    quizSelectOptions.classList.remove("hide");	      
 } );
 
 
 finalscore.innerText = mostRecentScore;
 
 
-let counter = 10;
-
- setInterval(function()  {
-    counter--;
-
-    if (counter >= 0) {
-        id = document.querySelector('#count');
-        id.innerHTML = counter;
-    }
-     if  (counter === 0) {
-            id.innerHTML = 'Times Up!';           
-            counter = 10;
-            fetchNewQuestion();
-           
-    } 
-
-}, 1000); 
 
